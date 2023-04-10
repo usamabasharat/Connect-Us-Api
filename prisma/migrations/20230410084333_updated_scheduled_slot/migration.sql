83e0e43a210ac9ce7ee27f217e40616f38a889e5
Sibghat Ullah <sibghat.athar@devsinc.com>
2023-04-10 14:39:20 +0500

    created meeting & schedule slot integrated with backend

---
+++ prisma/migrations/20230410084333_updated_scheduled_slot/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `type` on the `scheduled_slots` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "scheduled_slots" DROP COLUMN "type";
